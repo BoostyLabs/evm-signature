5094d9eca0753b9c34f8476e653638bf512c6d65
Dmytro <dmytroyakimuk@gmail.com>
2022-03-31 14:00:51 +0300

    fixed DeployNFTContract

--- deploy.go
+++ deploy.go
@@ -5,7 +5,6 @@ package evmsignature
 
 import (
 	"context"
-	"crypto/ecdsa"
 	"math/big"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
@@ -23,7 +22,6 @@ type ContractConfig struct {
 	AddressNodeServer string   `json:"addressNodeServer"`
 	PrivateKey        string   `json:"privateKey"`
 	ChainID           *big.Int `json:"chainID"`
-	GasLimit          uint64   `json:"gasLimit"`
 }
 
 // DeployNFTContract deploys nft contract for each collection nfts.
@@ -38,31 +36,11 @@ func DeployNFTContract(ctx context.Context, config ContractConfig, deployContrac
 		return common.Address{}, ErrDeployContract.Wrap(err)
 	}
 
-	publicKey := privateKeyECDSA.Public()
-	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
-	if !ok {
-		return common.Address{}, ErrDeployContract.New("error casting public key to ECDSA")
-	}
-
-	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
-	nonce, err := client.PendingNonceAt(ctx, fromAddress)
-	if err != nil {
-		return common.Address{}, ErrDeployContract.Wrap(err)
-	}
-
-	gasPrice, err := client.SuggestGasPrice(ctx)
-	if err != nil {
-		return common.Address{}, ErrDeployContract.Wrap(err)
-	}
-
 	auth, err := bind.NewKeyedTransactorWithChainID(privateKeyECDSA, config.ChainID)
 	if err != nil {
 		return common.Address{}, ErrDeployContract.Wrap(err)
 	}
-	auth.Nonce = big.NewInt(int64(nonce))
-	auth.Value = big.NewInt(0)              // in wei
-	auth.GasLimit = uint64(config.GasLimit) // in units
-	auth.GasPrice = gasPrice
+	auth.Value = big.NewInt(0) // in wei.
 
 	address, err := deployContract(auth, client)
 	return address, ErrDeployContract.Wrap(err)
